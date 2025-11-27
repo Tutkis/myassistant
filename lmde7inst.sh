651160e88113b1765537a6d52ab5b7e70493431c
Sergey <34441740+Tutkis@users.noreply.github.com>
2025-11-27 22:15:43 +0700

    Delete lmde7inst.sh

--- lmde7inst.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt upgrade -y && \
-wget -O /tmp/package.deb https://мойассистент.рф/скачать/Download/1375 && \
-sudo dpkg -i /tmp/package.deb && rm -f /tmp/package.deb
